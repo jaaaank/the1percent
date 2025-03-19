77722a90912eb753ae32061fbfd02af6fad98eec
jamnk <pancakes3825@gmail.com>
2025-03-19 14:40:46 -0400

    stuff and stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GameManager : MonoBehaviour
 {
     public float money;
     public string playerName;
+    public float upperClassApproval;
+    public float lowerClassApproval;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +20,20 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void buttonPressed(bool choice)
+    {
+        if (choice) {
+            upperClassApproval += 1;
+            lowerClassApproval -= 1;
+        }
+
+        if (!choice) {
+            lowerClassApproval -= 1;
+            lowerClassApproval += 1;
+        }
+        print("Lower Class Approval: " + lowerClassApproval);
+        print("Upper Class Approval: " + upperClassApproval);
     }
 }
