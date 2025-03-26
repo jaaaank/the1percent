5967627e27a11eef63695b905fc2cf6afd4f4949
jamnk <pancakes3825@gmail.com>
2025-03-26 14:56:45 -0400

    we're reworking literally everything lmfao

---
+++ Assets/Scripts/County.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class County : MonoBehaviour
+{
+    public float hapiness;
+
+    public float wealth;
+    public float food;
+    public float meds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void updateHapiness()
+    {
+        hapiness = (wealth+food+meds)/3;
+        Vector3 color = Vector3.Lerp(new Vector3(1, 0, 0), new Vector3(0, 1, 0), hapiness / 100);
+        gameObject.GetComponent<SpriteRenderer>().color = new Vector4(color.x, color.y, color.z, 1);
+    }
+}
